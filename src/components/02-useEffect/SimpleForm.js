338eb7fed92895bef861b79faf7dd7c1bfa76ff5
Camilo López <camiloj.lopez79@gmail.com>
2022-03-31 15:21:54 -0300

    useEffect - SimpleForm

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from "react";
+import './effects.css';
+
+export const SimpleForm = () => {
+  const [ formState, setFormState ] = useState({
+    name:'',
+    email: ''
+  });
+
+  const { name, email } = formState;
+
+  useEffect( () => {
+    console.log('Hey');
+  },[]);
+  useEffect( () => {
+    console.log('formState Cambió');
+  },[ formState ]);
+  /*
+  // El useEffect se dispara cada vez que cambia el estado de la aplicación. En este caso, si queremos prevenir ese comportamiento, tenemos que enviar un arreglo vacio como segundo parametro
+  useEffect( () => {
+    console.log('Hey');
+  }, []);
+  // También se puede estar escuchando cualquier elemento del componente
+  useEffect( () => {
+    console.log('formState Cambió');
+  },[ email ]);
+  */
+
+  const handleInputChange = ({ target }) => {
+    setFormState({
+      ...formState,
+      [ target.name ]: target.value
+    });
+  }
+
+  return (
+    <>
+      <h1>useEffect</h1>
+      <hr />
+
+      <div className='from-group'>
+        <input 
+          type='text'
+          name='name'
+          className='from-control'
+          placeholder='Escribe tu nombre'
+          autoComplete='off'
+          value={ name }
+          onChange={ handleInputChange }
+        />
+      </div>
+
+      <div className='from-group'>
+        <input 
+          type='text'
+          name='email'
+          className='from-control'
+          placeholder='Escribe tu Email'
+          autoComplete='off'
+          value={ email }
+          onChange={ handleInputChange }
+        />
+      </div>
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom';
 //import { HooksApp } from './HooksApp';
 // Components
 //import { CounterApp } from './components/01-useState/CounterApp';
-import { CounterCustom } from './components/01-useState/CounterCustom';
+//import { CounterCustom } from './components/01-useState/CounterCustom';
+import {SimpleForm} from './components/02-useEffect/SimpleForm';
 
 ReactDOM.render(
-  <CounterCustom />,
+  <SimpleForm />,
   document.getElementById('root')
 );
