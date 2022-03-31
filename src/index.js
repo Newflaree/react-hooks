6a67a84b37386af4be30ba4c0bc63fa1102a224b
Camilo López <camiloj.lopez79@gmail.com>
2022-03-31 11:23:48 -0300

    useState

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // App
-import { HooksApp } from './HooksApp';
+//import { HooksApp } from './HooksApp';
+// Components
+import { CounterApp } from './components/01-useState/CounterApp';
 
 ReactDOM.render(
-  <HooksApp />,
+  <CounterApp />,
   document.getElementById('root')
 );
